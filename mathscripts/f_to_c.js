500bbe69f3984ebb012539f94687e48996244048
melzmatz <melendezdiana95@gmail.com>
2023-10-18 13:04:04 -0700

    ex1

---
+++ mathscripts/f_to_c.js
@@ -0,0 +1,5 @@
+const fahrenheitTemp = 115.2;
+
+const celsiusTemp = ((fahrenheitTemp - 32) * 5) / 9;
+
+console.log(`${fahrenheitTemp} degrees Fahrenheit is approximately ${celsiusTemp.toFixed(1)} degrees Celsius.`);
---
+++ mathscripts/taxes.js
@@ -0,0 +1,7 @@
+const annualSalary = 50000
+
+const monthlyfedTaxes = .23
+
+const monthlyTaxwithheld = (annualSalary / 12) * monthlyfedTaxes
+
+console.log(`The monthly tax withheld is: $${monthlyTaxwithheld.toFixed(0)}`)
