24ee02e36dfee0ea060cf6c0c970b73927af8b42
melzmatz <melendezdiana95@gmail.com>
2023-10-19 15:56:09 -0700

    major

---
+++ mathscripts/ifscripts.js/show_major.js
@@ -0,0 +1,18 @@
+let studentName = "Bad Bunny"
+let studentMajor = "HIST"
+
+let major = ""
+let location = ""
+
+switch (studentMajorcode) {
+    case "BIO":
+    major = "Biology"
+    location = " Science Building, room 310"
+    break
+    case "CSCI":
+    major = "Computer Science"
+    location = "Shepard Hall, room 314"
+    break
+    case " ENG":
+
+}
