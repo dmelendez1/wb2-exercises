8c9b57d6f5b4e8b384d4a5009970874f0d5a8cf9
melzmatz <melendezdiana95@gmail.com>
2023-10-20 15:27:41 -0700

    work

--- greetbyname/scripts/index.js
+++ greetbyname/scripts/index.js
@@ -1,20 +1,20 @@
 
 function onGreetUserBtnClicked() {
-    const nameInput = document.getElementById('nameInput');
+    const nameInput = document.getElementById('nameInput')
     const userName = nameInput.value;
     if (userName.trim() === '') {
-        alert('Please enter your name');
+        alert('Please enter your name')
     } else {
-        const greetingMessage = ` Hello ${userName}, welcome`;
+        const greetingMessage = ` Hello ${userName},`
         alert(greetingMessage);
     }
 }
 
 
 function init() {
-    const greetButton = document.getElementById('greetBtn');
+    const greetButton = document.getElementById('greetBtn')
 
-    greetButton.addEventListener('click', onGreetUserBtnClicked);
+    greetButton.addEventListener('click', onGreetUserBtnClicked)
 }
 
 window.onload = init
