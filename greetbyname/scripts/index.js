e956dd1369e2487651212bfc061ce3d2482514ec
melzmatz <melendezdiana95@gmail.com>
2023-10-20 15:59:21 -0700

    button works

--- greetbyname/scripts/index.js
+++ greetbyname/scripts/index.js
@@ -1,20 +1,18 @@
-
 function onGreetUserBtnClicked() {
-    const nameInput = document.getElementById('nameInput')
-    const userName = nameInput.value;
-    if (userName.trim() === '') {
-        alert('Please enter your name')
-    } else {
-        const greetingMessage = ` Hello ${userName},`
-        alert(greetingMessage);
-    }
+  const nameInput = document.getElementById("nameInput");
+  const userName = nameInput.value;
+  if (userName.trim() === "") {
+    alert("Please enter your name");
+  } else {
+    const greetingMessage = ` Hello ${userName},`;
+    alert(greetingMessage);
+  }
 }
-
 
 function init() {
-    const greetButton = document.getElementById('greetBtn')
+  const greetButton = document.getElementById("greetBtn");
 
-    greetButton.addEventListener('click', onGreetUserBtnClicked)
+  greetButton.addEventListener("click", onGreetUserBtnClicked);
 }
 
-window.onload = init
+window.onload = init;
