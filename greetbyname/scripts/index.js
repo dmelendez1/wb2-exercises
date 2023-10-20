1c23fe23cf8fc2cd463956a3b168611f2ce4af2d
melzmatz <melendezdiana95@gmail.com>
2023-10-20 15:16:30 -0700

    created user greet btn page

---
+++ greetbyname/scripts/index.js
@@ -0,0 +1,20 @@
+
+function onGreetUserBtnClicked() {
+    const nameInput = document.getElementById('nameInput');
+    const userName = nameInput.value;
+    if (userName.trim() === '') {
+        alert('Please enter your name');
+    } else {
+        const greetingMessage = ` Hello ${userName}, welcome`;
+        alert(greetingMessage);
+    }
+}
+
+
+function init() {
+    const greetButton = document.getElementById('greetBtn');
+
+    greetButton.addEventListener('click', onGreetUserBtnClicked);
+}
+
+window.onload = init
